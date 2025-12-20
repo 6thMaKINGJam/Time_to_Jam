d347bf13e474924d7c091ec726262a768443bf30
neoy2j <iyeonjae2343@ewha.ac.kr>
2025-12-20 13:51:01 +0900

    feat: 01_Prologue 집 배경 연결

--- Assets/Yeonjae/PrologueManager.cs
+++ Assets/Yeonjae/PrologueManager.cs
@@ -6,17 +6,27 @@ public class PrologueManager : MonoBehaviour
 {
     public GameObject messageCanvas;
     public GameObject mailCanvas;
+    public GameObject houseCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
         messageCanvas.SetActive(true);
         mailCanvas.SetActive(false);
+        houseCanvas.SetActive(false);
     }
 
     public void OpenMail()
     {
         messageCanvas.SetActive(false);
         mailCanvas.SetActive(true);
+        houseCanvas.SetActive(false);
+    }
+
+    public void OpenHouse()
+    {
+        messageCanvas.SetActive(false);
+        mailCanvas.SetActive(false);
+        houseCanvas.SetActive(true);
     }
 }
